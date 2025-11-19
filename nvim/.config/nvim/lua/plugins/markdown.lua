ad4d5ca94c89e2816c1ec018a4477c00d0851af1
tuanpnt17 <tuanpntse173039@fpt.edu.vn>
2025-11-20 00:17:55 +0700

    feat(nvim): update plugins and obsidian config
    - Updated plugin lockfile versions for several Neovim plugins.
    - Removed Copilot Native from LazyVim extras.
    - Added custom color variables to markdown plugin config.
    - Enabled Obsidian footer and statusline, added UI options.
    - Added keymaps for pasting images and opening backlinks in Obsidian.
    - Updated Hyprland workspace 11 comment to "Z: Zalo".

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,16 @@
+local color1_bg = "#1E3A5F"
+local color2_bg = "#3A2F1F"
+local color3_bg = "#1E3A1F"
+local color4_bg = "#1F3A3A"
+local color5_bg = "#2F1E3A"
+local color6_bg = "#2F1E3A"
+local color_fg = "#323449"
+
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    opts = {},
+    opts = {
+    },
   },
 }
--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -26,7 +26,17 @@ return {
         default_tags = { "daily" },
       },
       footer = {
-        enabled = false,
+        enabled = true,
+      },
+      ui = {
+        enable = false,
+        ignore_conceal_warn = false,
+        update_debounce = 200,
+        max_file_length = 5000,
+      },
+      statusline = {
+        format = "{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars",
+        enabled = true,
       },
     },
     config = function(_, opts)
@@ -34,6 +44,8 @@ return {
 
       -- ✅ Global keymaps (ngoài buffer)
       vim.keymap.set("n", ";n", "<cmd>ObsidianNew<cr>", { desc = "New note (Inbox)" })
+      vim.keymap.set("n", ";i", "<cmd>Obsidian paste_img<cr>", { desc = "Paste copied image" })
+      vim.keymap.set("n", ";b", "<cmd>ObsidianBacklinks<cr>", { desc = "Open Backlinks" })
       vim.keymap.set("n", ";t", "<cmd>ObsidianToday<cr>", { desc = "Today journal" })
       vim.keymap.set("n", ";y", "<cmd>ObsidianYesterday<cr>", { desc = "Yesterday journal" })
       vim.keymap.set("n", ";m", "<cmd>ObsidianTomorrow<cr>", { desc = "Tomorrow journal" })
